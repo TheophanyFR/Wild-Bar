9b0db7d2755be5cbd31fdbe4c898c78bb41c08d3
Thibaut Decherit <thibaut.decherit@gmail.com>
2017-09-20 15:00:52 +0200

    added back to top button

--- js/function.js
+++ js/function.js
@@ -1,14 +1,43 @@
-var affixElement = '#navbar-main';
+$(document).ready(function () {
 
-$(affixElement).affix({
-    offset: {
-        // Distance of between element and top page
-        top: function () {
-            return (this.top = $(affixElement).offset().top)
-        },
-        // when start #footer
-        bottom: function () {
-            return (this.bottom = $('#footer').outerHeight(true))
+// début partie navbar
+
+    var affixElement = '#navbar-main';
+
+    $(affixElement).affix({
+        offset: {
+            // Distance of between element and top page
+            top: function () {
+                return (this.top = $(affixElement).offset().top)
+            },
+            // when start #footer
+            bottom: function () {
+                return (this.bottom = $('#footer').outerHeight(true))
+            }
         }
-    }
+    });
+
+// fin partie navbar
+
+// début bouton go back to top
+
+    var amountScrolled = 300;
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#goToTopBtn').fadeIn('slow');
+        } else {
+            $('#goToTopBtn').fadeOut('slow');
+        }
+    });
+    $('#goToTopBtn').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+
+
+// fin bouton go back to top
 });
